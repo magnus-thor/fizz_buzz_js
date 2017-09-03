51daa7c3354e23df4e88dc1c52ddcec5bb076a89
magnus-thor <magnus.thor.ar@gmail.com>
2017-09-03 16:46:35 +0200

    sad path done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
-
-var onReadyEvents = function(){
-  $('#click_me').click(function() {
-    var text = $('#text').val();
-    $('#display_message').html(text);
-  });
-}
+// 
+// var onReadyEvents = function(){
+//   $('#click_me').click(function() {
+//     var text = $('#text').val();
+//     $('#display_message').html(text);
+//   });
+// }
--- assets/js/fizz_buzz_ui.js
+++ assets/js/fizz_buzz_ui.js
@@ -2,9 +2,9 @@ $(document).ready(function () {
 
   $('#run_fizz').click(function() {
     var n = parseInt($('#number').val());
+    var fizzbuzz = new Fizz_buzz();
     if ($.isNumeric(n) && n > 0) {
       var x = 1;
-      var fizzbuzz = new Fizz_buzz();
       var stop_buzz =  setInterval(function() {
         fizzbuzz.normal_game(x);
         $('#display_buzz').css("color", fizzbuzz.use_color).text(fizzbuzz.to_screen);
@@ -13,6 +13,9 @@ $(document).ready(function () {
         }
         x++;
       }, 500);
+    } else {
+        fizzbuzz.setMessage('Please enter an Integer');
+        $('#display_buzz').css("color", fizzbuzz.use_color).text(fizzbuzz.to_screen);
     }
   });
 
